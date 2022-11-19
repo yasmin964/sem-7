58fffd34599ab1215703c489bd541ea64db879ba
yasya <kalgexavva1970@icloud.com>
2022-11-19 16:29:28 +0300

    add new file

---
+++ ex2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте двумерный массив размера m на n,
+//каждый элемент в массиве находится по формуле: Aₙₙ = m+n.
+//Выведите полученный массив на экран.
+
+int[,] GetArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int [,] inArray)
+{
+    for (int i=0;i<inArray.GetLength(0);i++)
+    {
+        for (int j=0;j<inArray.GetLength(1);j++)
+        {
+            Console.Write($"{inArray[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во стобцов: ");
+int cols = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, cols);
+PrintArray(array);
