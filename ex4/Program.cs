12aa5749843da87416baeab1c5410ad3788c4f32
yasya <kalgexavva1970@icloud.com>
2022-11-19 16:29:39 +0300

    add new file

---
+++ ex4/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 51: Задайте двумерный массив.
+//Найдите сумму элементов, находящихся на главной диагонали (с
+//индексами (0,0); (1;1) и т.д
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int DiaganolSum (int[,] inArray)
+{
+    int sum = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        if (i >= inArray.GetLength(1)) break;
+
+        sum += inArray[i,i];
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов: ");
+int cols = int.Parse(Console.ReadLine()!);
+
+int[,] WorkingArray = GetArray(rows, cols, 0, 10);
+PrintArray(WorkingArray);
+
